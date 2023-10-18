80766adba75632d73db392663daab1b23ed3b690
Nataly Martínez <nataly.97.11.m@gmail.com>
2023-10-17 22:08:02 -0500

    ajustes

---
+++ README.md
@@ -0,0 +1,2 @@
+<!-- Link de imagenes -->
+https://monosnap.com/list/652ef1adaf3a730eb6de3b48
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const fs = require("fs"); 
 const contacts = require("./contacts");
-const readline = require("readline");
-
 const { Command } = require("commander");
 const program = new Command();
 
@@ -15,51 +12,27 @@ program
 program.parse(process.argv);
 const argv = program.opts();
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout, 
-})
-
-
 // TODO: refactorizar
 function invokeAction({ action, id, name, email, phone }) {
-    rl.question(`Escriba una opción:\n list --Para tabla de contactos\n get --Para filtrar contacto por ID\n add --Para agregar contacto\n remove --Para eliminar contacto\n`, (action) => {
-        switch (action) {
-            case "list":
-                contacts.listContacts();
-                invokeAction(argv);
-                break;
-            case "get":
-                // ... id
-                rl.question(`Ingrese el ID del contacto:\n`, (id) => {
-                    contacts.getContactById(id);
-                    invokeAction(argv);
-                })
-                break;
-            case "add":
+    switch (action) {
+        case "list":
+            contacts.listContacts();
+            break;
+        case "get":
+            // ... id
+                contacts.getContactById(id);
+            break;
+        case "add":
             // ... name email phone
-            case "add":
-                rl.question("Ingrese el nombre del contacto: ", (name) => {
-                    rl.question("Ingrese el correo del contacto: ", (email) => {
-                        rl.question("Ingrese el teléfono del contacto: ", (phone) => {
-                            contacts.addContact(name, email, phone);
-                            invokeAction(argv);
-                        });
-                    });
-                });
-                break;
-            case "remove":
-                // ... id
-                rl.question(`Ingrese el ID del contacto a eliminar:\n`, (id) => {
-                    contacts.removeContact(id);
-                    invokeAction(argv);
-                })
-                break;
-            default:
-                console.warn("\x1B[31m Unknown action type!");
-        }
-    })
-
+            contacts.addContact(name, email, phone);
+            break;
+        case "remove":
+            // ... id
+            contacts.removeContact(id);
+            break;
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
 }
-
+    
 invokeAction(argv);
